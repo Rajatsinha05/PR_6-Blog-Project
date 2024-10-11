eded1f0a924f12b1be494eb41b3f7978e194a626
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-10-11 11:11:43 +0530

    Update db.js

--- blog/config/db.js
+++ blog/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 const connection=async()=>{
-    await mongoose.connect("mongodb+srv://daksh1or2:movie@cluster0.qy1gavv.mongodb.net/?retryWrites=true&w=majority")
+    await mongoose.connect("mongodb+srv://rw5rajatas:node@cluster0.mjizt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     console.log("mongodb connected");
 }
 module.exports=connection
